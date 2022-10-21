699e2b7b45460dda7095fd84454f44a49567a8fd
Avinash S Prabhu <avi.g751@gmail.com>
2022-10-21 12:16:03 +0400

    fps

---
+++ Assets/Fps Controller/Fps Scripts/FpsMovment.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FpsMovment : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] int MoveSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movment();
+    }
+
+    void Movment()
+    {
+        float MoveX = Input.GetAxis("Horizontal");
+        float MoveZ = Input.GetAxis("Vertical");
+        Vector3 Direction = new Vector3 (MoveX, MoveZ, 0);
+        
+        rb.velocity = (transform.right * Direction.x + transform.forward * Direction.y) * MoveSpeed;
+
+    }
+}

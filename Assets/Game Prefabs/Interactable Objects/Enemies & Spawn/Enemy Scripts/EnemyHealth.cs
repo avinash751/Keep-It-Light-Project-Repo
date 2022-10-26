4d7a1ab5c2dd38ffab7b095b3acbe80ef7252c2f
Avi_156 <avi.g751@gmail.com>
2022-10-26 15:16:39 +0400

    asset files reorganized

--- Assets/Game Prefabs/Interactable Objects/Enemies & Spawn/Enemy Scripts/EnemyHealth.cs
+++ Assets/Game Prefabs/Interactable Objects/Enemies & Spawn/Enemy Scripts/EnemyHealth.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyHealth :ObjectHealth,IDestroyable
+public class EnemyHealth:ObjectHealth,IDestroyable
 {
     public override void DestroyObject()
     {

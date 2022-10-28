7db302943ee76749ef1a743243cad14141a206d3
Avi_156 <avi.g751@gmail.com>
2022-10-28 16:58:35 +0400

    enemies spawn in a radius, radius tool made, light orb can now destroy spawner

--- Assets/Duro's Folder/Scripts/Enemy_Spawn.cs
+++ Assets/Duro's Folder/Scripts/Enemy_Spawn.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class Enemy_Spawn : MonoBehaviour
+public class Enemy_Spawn : MonoBehaviour,IDestroyable
 {
 
     public GameObject SpawnEnemy;
+    [Range(1, 10)][SerializeField] float SpawnRate;
+    public int SpawnRadius;
 
     // Start is called before the first frame update
     void Start()
@@ -15,21 +18,55 @@ public class Enemy_Spawn : MonoBehaviour
 
     void InstantiateEnemy()
     {
+        Vector3 SpawnPosition = transform.position;
+
         GameObject GO_Current = (GameObject)Instantiate(SpawnEnemy);
-        GO_Current.transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z);
+        Vector2 CircleRadius =  Random.insideUnitCircle * SpawnRadius;
+        GO_Current.transform.position = new Vector3( transform.position.x +CircleRadius.x,transform.position.y, transform.position.z + CircleRadius.y);
 
         StartCoroutine("waitForFewSeconds");
     }
 
     IEnumerator waitForFewSeconds()
     {
-        yield return new WaitForSeconds(7.0f);
+        yield return new WaitForSeconds(SpawnRate);
         InstantiateEnemy();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    // debug drawing editor code 
+    private void OnDrawGizmos()
     {
-        
+        UnityEditor.Handles.color = new Color(0, 0, 0, 0.15f);
+        UnityEditor.Handles.DrawSolidDisc(transform.position, Vector3.up, SpawnRadius);
+
+        UnityEditor.Handles.color = Color.white;
+        UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.down, SpawnRadius,2.5f);
+
+        GUIStyle Bold = new GUIStyle();
+        Bold.richText = true;
+        Bold.fontStyle = FontStyle.Bold;
+        Bold.normal.textColor = Color.white;
+        UnityEditor.Handles.Label(transform.position + new Vector3(0, 2, 0), gameObject.name,Bold);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        UnityEditor.Handles.color = new Color(255, 0, 0, 0.4f);
+        UnityEditor.Handles.DrawSolidDisc(transform.position, Vector3.up, SpawnRadius);
+
+        UnityEditor.Handles.color = Color.white;
+        UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.down, SpawnRadius, 2.5f);
+
+        GUIStyle Bold = new GUIStyle();
+        Bold.richText = true;
+        Bold.fontStyle = FontStyle.Bold;
+        Bold.normal.textColor = Color.black;
+        UnityEditor.Handles.Label(transform.position + new Vector3(0, 2, 0), gameObject.name, Bold);
+    }
+
+    public virtual void DestroyObject()
+    {
+        Destroy(gameObject);
     }
 }

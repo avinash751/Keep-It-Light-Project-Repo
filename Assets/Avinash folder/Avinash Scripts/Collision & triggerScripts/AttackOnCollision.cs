e2bb1b70e3cdbc36addb118f3a7584c93214cd35
Avi_156 <avi.g751@gmail.com>
2022-10-28 17:00:27 +0400

    collision script

--- Assets/Avinash folder/Avinash Scripts/Collision & triggerScripts/AttackOnCollision.cs
+++ Assets/Avinash folder/Avinash Scripts/Collision & triggerScripts/AttackOnCollision.cs
@@ -1,14 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class AttackOnCollision : MonoBehaviour
 {
     [SerializeField] int AmountOfDamage;
     public bool DoMoreOnCollsion;
-    static UnityAction DoExtraOnCollision;
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.GetComponent<IDamagable>() != null)
